0053a59bdf9aff814152f90eb10bdd7073c14df3
nenekelvin <karvinmitchar@gmail.com>
2020-05-31 14:29:45 +0300

    remove sqlite gem replace with pg

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 import Recipes from "../components/Recipes";
-import Recipes from "../components/Recipes";
+import Recipe from "../components/Recipe";
 
 export default (
   <Router>
