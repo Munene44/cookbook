b0c7007de43a9d67dddd55d78f65ca675338604d
nenekelvin <karvinmitchar@gmail.com>
2020-05-31 13:37:03 +0300

    constructor to render react

--- app/javascript/components/Recipes.jsx
+++ app/javascript/components/Recipes.jsx
@@ -1,2 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+
+class Recipes extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: []
+    };
+  }
+
+}
+export default Recipes;
